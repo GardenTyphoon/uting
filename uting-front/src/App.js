adcb0562092dd8bda3d9b07b1b76f5cf04e64178
Sumin Hong <sumiiniii@ajou.ac.kr>
2021-04-02 12:44:56 +0900

    firebase init

--- uting-front/src/App.js
+++ uting-front/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter, Router, Route, Switch } from "react-router-dom";
 
+import IntroPage from './component/IntroPage'
+import SignUp from './component/SignUp'
+import SignIn from './component/SignIn'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={SignIn}></Route>
+          <Route path="/signup" component={SignUp}></Route>
+        </Switch>
+          
+      </BrowserRouter>
+          
     </div>
   );
 }
