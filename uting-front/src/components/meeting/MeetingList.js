4c41e4c4a510de57fe5be47578c4b4084b34df16
unknown <zztiok@ajou.ac.kr>
2021-04-22 22:51:51 +0900

    미팅방 정보 출력

--- uting-front/src/components/meeting/MeetingList.js
+++ uting-front/src/components/meeting/MeetingList.js
@@ -1,6 +1,18 @@
 import React, { useState,useEffect } from 'react';
 import axios from 'axios';
-
+import woman from '../../img/woman.png'
+import man from '../../img/man.png'
+function mannerCredit(avgManner){
+    if(avgManner === 4.5) return "A+";
+    else if(avgManner<4.5 && avgManner >=4.0) return "A0";
+    else if(avgManner<4.0 && avgManner >=3.5) return "B+";
+    else if(avgManner<3.5 && avgManner >=3.0) return "B0";
+    else if(avgManner<3.0 && avgManner >=2.5) return "C+";
+    else if(avgManner<2.5 && avgManner >=2.0) return "C0";
+    else if(avgManner<2.0 && avgManner >=1.5) return "D+";
+    else if(avgManner<1.5 && avgManner >=1.0) return "D0";
+    else return "F";
+}
 export default function MeetingList() {
     const [viewRoomList, setView] = useState([]);
     const attendRoomByID = (room, index) => {
@@ -22,6 +34,22 @@ export default function MeetingList() {
       },[]);
      
     return (//tr map 한다음에 key넣어주기
+        <div>
+            {viewRoomList.map((room,index)=>
+                <div>
+                    <div>{room.title}</div>
+                    <div>{room.avgManner}</div>
+                    
+                    <div>{mannerCredit(room.avgManner)}</div>
+                    <div>{room.avgAge}</div>
+                    <img style={{ width: "15px", height: "20px" }}src={woman} />
+                    <img style={{ width: "15px", height: "20px" }}src={man} />
+                    <div>{room.numOfWoman} | {room.numOfMan}</div>
+                    <button onClick={()=>attendRoomByID(room,index)}>참가</button>
+                </div>
+            )}
+        </div>
+        /*
         <React.Fragment>
             <h2>방리스트</h2>
             <table border="1">
@@ -42,6 +70,7 @@ export default function MeetingList() {
                     )}
             </table>
         </React.Fragment>
+        */
         
     )
 }
