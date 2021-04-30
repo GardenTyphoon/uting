468e6b2409f286f060c3a482080b154e9347c776
unknown <zztiok@ajou.ac.kr>
2021-05-01 01:43:35 +0900

    참가 버튼 클릭 시 페이지 이동 및 mc봇 컴포넌트 추가

--- uting-front/src/components/mc/McBot.js
+++ uting-front/src/components/mc/McBot.js
@@ -1,15 +1,28 @@
-import React, { useState,useEffect } from 'react';
-import { Route, Link,Switch,Router } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Route, Link, Switch, Router } from 'react-router-dom';
 import axios from 'axios';
-import { Button } from 'reactstrap';
-
+import McBotImg from '../../img/mc봇.png';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 const McBot = () => {
-  
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const toggle = (e) => setDropdownOpen(prevState => !prevState);
+
   return (
     <div>
-        Mc봇 컴포넌트
-        
+
+
+      <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+        <DropdownToggle caret style={{ width:"15%", backgroundColor: "transparent", border: "0px" }}>
+          <img src={McBotImg} style={{ width: "80%" }} />
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem>게임 추천</DropdownItem>
+          <DropdownItem>대화 추천</DropdownItem>
+          <DropdownItem>음악 재생</DropdownItem>
+          <DropdownItem>실시간 게임</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
     </div>
   );
 };
