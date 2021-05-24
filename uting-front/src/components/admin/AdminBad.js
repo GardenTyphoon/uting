56ed5faf1d974f4896a344a2eb7d2767fc5ff24f
Sumin Hong <sumiiniii@ajou.ac.kr>
2021-05-24 21:00:25 +0900

    신고 리스트 관리자페이지에서 추출하기

--- uting-front/src/components/admin/AdminBad.js
+++ uting-front/src/components/admin/AdminBad.js
@@ -37,6 +37,7 @@ const FlexBox = styled.div`
 const AdminBad = () => {
     const [open, setOpen] = useState(false);
     const [reportedList,setReportedList]=useState([])
+    const [choice,setChoice]=useState({})
     let isOpen = () => {
         setOpen(!open);
       };
@@ -55,6 +56,14 @@ const AdminBad = () => {
           getReported()
         }
       },[open])
+
+      let goHell = async(e)=>{
+        console.log("나쁜사람",choice)
+      }
+
+      let pass = async(e)=>{
+        console.log("봐주기",choice)
+      }
     
   return (
     <Card>
@@ -70,8 +79,9 @@ const AdminBad = () => {
       <CardBody>
         <div style={{width:"100%",height:"100%"}}>
           <span>신고자 목록</span>
+          <Button color="danger" onClick={(e)=>goHell(e)}>악한사람</Button><Button color="warning" onClick={(e)=>pass(e)}>봐주기</Button>
           <div>{reportedList.map((per,i)=>{
-            return(<div>{per.requester}-{per.target}-{per.content}</div>)
+            return(<div onClick={(e)=>setChoice(per)}>신고한사람:{per.requester}-신고된사람{per.target}-신고사유{per.content}</div>)
           })}</div>
         </div>
 
