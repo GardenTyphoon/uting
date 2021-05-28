3818e7baec8d61400c7d05b4b554111540a96f89
Sumin Hong <sumiiniii@ajou.ac.kr>
2021-05-28 15:45:16 +0900

    관리자 페이지 광고 및 기타 문의 이미지 출력

--- uting-front/src/components/admin/AdminAd.js
+++ uting-front/src/components/admin/AdminAd.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import classnames from 'classnames';
 import Conversation from './Conversation'
 import GameRecom from './GameRcom'
+import ProfileNoImage from "../../img/ProfileNoImage.jpg";
 import {Button,
     ButtonGroup,
     Card,
@@ -29,6 +30,7 @@ import {Button,
     Nav,
     NavItem,
     NavLink,
+    Fade,
   } from 'reactstrap';
 
 
@@ -45,7 +47,21 @@ import {Button,
 const AdminAd = () => {
     const [open, setOpen] = useState(false);
     const [requestList,setRequestList]=useState([]);
-
+    const [fadeIn, setFadeIn] = useState(false);
+    const [clickData,setClickData]=useState({})
+    const [imgBase64, setImgBase64] = useState("");
+    const fadetoggle = (e,i) => {
+      setFadeIn(!fadeIn)
+      let data={
+        num:i,
+        email:e.email,
+        file:e.file,
+        contents:e.contents
+      }
+      setClickData(data)
+      let staticpath = "http://localhost:3001";
+      setImgBase64(staticpath + e.file);
+    };
     let isOpen = () => {
         setOpen(!open);
       };
@@ -64,6 +80,10 @@ const AdminAd = () => {
         getAd()
       }
     },[open])
+    /*
+                      <Col >{data.name}</Col>
+                      <Col style={{marginLeft:"2%"}}>{data.email}</Col>   
+                      <Col style={{marginLeft:"2%"}}>{data.file}</Col> */
       
   return (
     <Card>
@@ -77,11 +97,42 @@ const AdminAd = () => {
     </CardHeader>
     <Collapse isOpen={open}>
       <CardBody>
-        {requestList.map((data,i)=>{
-          return(
-            <div>{data.type}{data.name}{data.email}{data.file}{data.title}{data.contents}</div>
-          )
-        })}
+
+      <div style={{width:"100%",height:"50%"}}>
+          <span className="reportedlist">문의 목록</span>
+          <Row className="header">
+            <Col style={{marginLeft:"10%"}}>문의 타입 </Col>
+            <Col style={{marginLeft:"2%"}}> 제목 </Col>
+            <Col style={{marginLeft:"1%"}}> 작성자 </Col>
+          </Row>
+          <div>{requestList.map((data,i)=>{
+            return(<><Row key={i} className="rowbutton" onClick={(e)=>fadetoggle(data,i)} >
+                      <Col style={{marginLeft:"10%"}}>{data.type==='Ad'?"광고":"기타"}</Col>
+                      <Col style={{marginLeft:"2%"}}>{data.title}</Col>  
+                      <Col style={{marginLeft:"2%"}}>{data.name}</Col>    
+                             
+                    </Row>
+                    <Row style={{marginLeft:"10%",marginRight:"50%",marginTop:"1%",marginBottom:"1%"}}>
+                    {clickData.num===i?
+                      <Fade  in={fadeIn}>
+                        <div style={{float:"left"}}>
+                        {imgBase64 === "" ? (
+                        <Col><img src={ProfileNoImage} alt="profile img" height="100" width="100" style={{ borderRadius: "10px"}} /></Col>
+                         ) : (
+                        <Col><img src={imgBase64} alt="profile img" height="100" width="130" style={{ borderRadius: "10px",float:"left" }} /> </Col>)}
+                        <Col style={{marginLeft:"20%",marginRight:"1000px"}}>{clickData.contents}</Col>
+                        <Col style={{marginLeft:"40%",marginRight:"1000px"}}>{clickData.email}</Col>
+                        </div>
+                      </Fade>:""}
+                    </Row>
+                    
+                    
+                  </>
+                  )
+          })}</div>
+          
+        </div>
+
 
        
       </CardBody>
