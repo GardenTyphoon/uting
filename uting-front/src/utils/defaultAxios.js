7aa5f102f198ba7c7f73379db2f22f9c0ad3f48f
Minje <minje@ajou.ac.kr>
2021-06-13 01:49:01 +0900

    api주소 수정

--- uting-front/src/utils/defaultAxios.js
+++ uting-front/src/utils/defaultAxios.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const defaultAxios = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "/api",
   //timeout: 1000,
 });
-
 
 // const defaultAxios = axios.create({
 //   baseURL: "http://localhost:3001",
