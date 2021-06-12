37c398847010860f12a7d3b36ad3e1eaa02b8304
minje0204 <minje@ajou.ac.kr>
2021-06-12 21:45:43 +0900

    귓말겜 alert 창 update

--- uting-front/src/utils/defaultAxios.js
+++ uting-front/src/utils/defaultAxios.js
@@ -2,11 +2,7 @@ import axios from "axios";
 
 const defaultAxios = axios.create({
   baseURL: "http://localhost:3001",
-<<<<<<< Updated upstream
   //timeout: 1000,
-=======
-  timeout: 1000,
->>>>>>> Stashed changes
 });
 
 export default defaultAxios;
