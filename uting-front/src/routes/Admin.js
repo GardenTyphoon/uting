02174f20bc772e02ba1c8cfe37a65dff85257cf2
Sumin Hong <sumiiniii@ajou.ac.kr>
2021-06-13 21:33:25 +0900

    관리자 대화추천 게임추천 수정

--- uting-front/src/routes/Admin.js
+++ uting-front/src/routes/Admin.js
@@ -53,7 +53,7 @@ const Admin = () => {
         <Content style={{ margin: "0 16px" }}>
           <Breadcrumb style={{ margin: "16px 0" }}>
             <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            <Breadcrumb.Item>Admin</Breadcrumb.Item>
           </Breadcrumb>
           <div style={{ padding: 24, background: "#fff", minHeight: 360 }}>
             {menustate === "MC봇 관리" ? (
