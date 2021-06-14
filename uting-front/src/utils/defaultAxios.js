0fce5c956cfa59807e65a5dffe52579c88f7bb3f
Sumin Hong <sumiiniii@ajou.ac.kr>
2021-06-14 17:05:01 +0900

    api

--- uting-front/src/utils/defaultAxios.js
+++ uting-front/src/utils/defaultAxios.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const defaultAxios = axios.create({
-  baseURL: "/api",
-  //timeout: 1000,
-});
-
 // const defaultAxios = axios.create({
-//   baseURL: "http://localhost:3001",
+//   baseURL: "/api",
 //   //timeout: 1000,
 // });
+
+const defaultAxios = axios.create({
+  baseURL: "http://localhost:3001",
+  //timeout: 1000,
+});
 export default defaultAxios;
