0b45afecd893642037b3005846ec98140dee4ad9
KEHP-K <rnjsxovnd@gmail.com>
2021-06-14 17:42:44 +0900

    qwer

--- uting-front/src/utils/baseurl.js
+++ uting-front/src/utils/baseurl.js
@@ -2,9 +2,9 @@ module.exports = {
   baseFront: "",
   baseBack: "/api",
 };
-/*
-module.exports = {
-  baseFront: "http://localhost:3000",
-  baseBack: "http://localhost:3001",
-};
-*/
+
+//module.exports = {
+//  baseFront: "http://localhost:3000",
+//  baseBack: "http://localhost:3001",
+//};
+
