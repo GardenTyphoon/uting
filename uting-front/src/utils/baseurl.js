13ee80f3bf170479c7ed5fb84527bf4654dc4715
Sumin Hong <sumiiniii@ajou.ac.kr>
2021-06-18 16:26:02 +0900

    삭제 버튼 활성화

--- uting-front/src/utils/baseurl.js
+++ uting-front/src/utils/baseurl.js
@@ -1,12 +1,12 @@
- 
+ /*
  module.exports = {
    baseFront: "",
    baseBack: "/api",
  };
+*/
 
 
-/*
  module.exports = {
    baseFront: "http://localhost:3000",
    baseBack: "http://localhost:3001",
- };*/
+ };
